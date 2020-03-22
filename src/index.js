dd3255c41ce10db960b2672fa814ad50653125df
Your Name <nicholasgrand@gmail.com>
2020-03-22 23:16:19 +0300

    added brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let myStr = str.split('');
+
+    if (myStr.length % 2 != 0) {
+        return false;
+    }
+
+    for (let i = 0; i < myStr.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (stack.length != 0 && stack[stack.length - 1] == bracketsConfig[j][0] && myStr[i] == bracketsConfig[j][1]) {
+                stack.pop();
+            }else if (myStr[i] == bracketsConfig[j][0]) {
+                stack.push(myStr[i]);
+            }
+        }
+    }
+    if (stack.length == 0) {
+        return true;
+    }else{
+        return false;
+    }
 }
